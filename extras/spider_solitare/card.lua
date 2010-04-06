b044fe63aa880f4cfe7b85ed1da0833b6637500a
James Graves <James Graves@localhost>
2010-04-06 11:22:39 -0500

    Started card module.

---
+++ extras/spider_solitare/card.lua
@@ -0,0 +1,16 @@
+
+--[[
+    Unlike the Haskell version, the cards and suites are represented
+    as simple integers.
+
+    cards: 1 == Ace, 2 == 2, ..., 12 == Queen, 13 == King
+    suits: 1 == Clubs, 2 == Diamonds, 3 == Hearts, 4 == Spades
+]]--
+
+card_regular_text = {   "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", 
+                        "Eight", "Nine", "Ten", "Jack", "Queen", "King" }
+
+card_board_text = { " A", " 2", " 3", " 4", " 5", " 6", " 7", 
+                    " 8", " 9", "10", " J", " Q", " K" }
+
+card_board_suit_text = { "C", "D", "H", "S" }
