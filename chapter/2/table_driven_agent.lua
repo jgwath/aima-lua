e4b2f46d23c55329d1ebbc025fddb51bacb1546a
James Graves <James Graves@localhost>
2010-04-17 12:07:17 +0000

    Fixed algorithm for table driven agent and test case.

--- chapter/2/table_driven_agent.lua
+++ chapter/2/table_driven_agent.lua
@@ -1,20 +1,21 @@
 #!/usr/bin/env lua
 
+require "utilities"
 local A = require "aima.agents"
 
 -- Simple test of table-driven agents.
 
 print("Table Driven agent:")
 
-local hist = { "foo",
-                x = { "bar" },
-                y = { "baz", z = {"foo"}, y = {"bar"}}
+local hist = { "buzz",
+                x = { "foo" },
+                y = { "bar", z = {"bingo"}, y = {"raffle"}}
                 }
 
 agent2 = A.Table_Driven_Agent{ "agent2", 2, hist }
 agent2 = A.Trace_Agent(agent2)
 
-percept_list = { "x", "y", "y", "z", "x", "z", "y", "y", "x" }
+percept_list = { "v", "x", "y", "y", "z", "x", "z", "y", "y", "x" }
 for _, v in ipairs(percept_list) do
     action = agent2:program(v)
 end
